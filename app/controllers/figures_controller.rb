b8e02e713617954d253453aab08329898c8760a6
Aida Abdikulova <abdikulova@gmail.com>
2019-12-12 05:38:30 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,6 +15,7 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(params[:figure])
     @figure.titles << Title.create(params[:title])
+    @figure.landmarks << Landmark.create(params[:landmark])
   end
 
   get '/figures/:id' do
